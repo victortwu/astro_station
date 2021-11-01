2603e63ecd31111c4d1f80438479e6592efe3320
victortwu <victor607@gmail.com>
2021-10-31 22:56:03 -0700

    started styleing the AstroCard, can now expand and close on click

--- src/components/AstroCard.js
+++ src/components/AstroCard.js
@@ -1,10 +1,27 @@
+import React, { useState } from 'react'
+import style from '../cssModules/astroCard.module.css'
+
+
+
 const AstroCard = (props) => {
 
+  const [expanded, setExpanded] = useState(false)
 
+  const expandDiv = expanded ? style.craftDown : style.craftUp
+
+  const btnVisibility = expanded ? style.hide : ''
+
+  console.log(expandDiv)
   return(
-    <div style={{backgroundColor: 'black', margin: '.5rem', padding: '.5rem'}}>
-    <h3>Name: {props.name}</h3>
-    <h5>Craft: {props.craft}</h5>
+    <div className={style.container}>
+      <div className={style.name}>
+        <h3>Name: {props.name}</h3>
+        <button onClick={()=> setExpanded(true)} className={`${style.dnBtn} ${btnVisibility}`}>V</button>
+      </div>
+      <div className={`${style.craft} ${expandDiv}`}>
+        <h5>Craft: {props.craft}</h5>
+        <button onClick={()=> setExpanded(false)} className={style.upBtn}>V</button>
+      </div>
     </div>
   )
 }
--- src/pages/Astronauts.js
+++ src/pages/Astronauts.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
 import AstroCard from '../components/AstroCard'
+import style from '../cssModules/astronauts.module.css'
 
 const Astronauts = (props) => {
 
@@ -12,14 +13,18 @@ console.log(props.astronauts)
 
   return(
     <div>
-      <h1>Astronuats in space</h1>
-      {props.astronauts.map((astro, i) => {
-        return(
-          <div key={i + astro.name}>
-          <AstroCard name={astro.name} craft={astro.craft} />
-          </div>
-        )
-      })}
+          <div className={style.title}><h1>Astronuats in space</h1></div>
+
+        <div className={style.cards}>
+
+          {props.astronauts.map((astro, i) => {
+            return(
+              <div key={i + astro.name}>
+              <AstroCard name={astro.name} craft={astro.craft} />
+              </div>
+            )
+          })}
+        </div>
     </div>
   )
 }
